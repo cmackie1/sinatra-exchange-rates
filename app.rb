b9f3f03796537d299c3dc5d33baafebc721e13b2
cmackie1 <165298882+cmackie1@users.noreply.github.com>
2024-04-18 17:13:15 +0000

    1

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  @raw = HTTP.get(@api_url)
+  @parsed = JSON.parse(@raw)
+  @currencies_hash = @parsed.fetch("currencies")
+  @currency_array = @currencies_hash.keys
+  erb(:base)
+end
+
+get ("/:cur") do
+  @cur = params.fetch("cur")
+  @api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  @raw = HTTP.get(@api_url)
+  @parsed = JSON.parse(@raw)
+  @currencies_hash = @parsed.fetch("currencies")
+  @currency_array = @currencies_hash.keys
+  erb(:currency)
+end
+
+get ("/:cur/:second_cur") do
+  @cur = params.fetch("cur")
+  @second_cur = params.fetch("second_cur")
+  @api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@cur}&to=#{@second_cur}&amount=1"
+  @raw = HTTP.get(@api_url)
+  @parsed = JSON.parse(@raw)
+  @result = @parsed.fetch("result")
+  erb(:conversion)
 end
